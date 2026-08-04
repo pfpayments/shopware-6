9d537d6c43f3a6c4b3cc85e9f5e121771062d6d4
andrewrowanwallee <andrew.rowan@wallee.com>
2026-08-04 10:44:00 +0200

    Release 6.2.4

--- src/Core/Api/WebHooks/Service/WebHooksService.php
+++ src/Core/Api/WebHooks/Service/WebHooksService.php
@@ -295,7 +295,7 @@ class WebHooksService {
 					->setNotifyEveryChange($data->isNotifyEveryChange())
 					->setState(CreationEntityState::CREATE)
 					->setEntityStates($data->getStates())
-					->setEnablePayloadSignatureAndState( true )
+					// ->setEnablePayloadSignatureAndState( true )
 					->setUrl($webHookUrlId);
 
 				$returnValue[] = $this->apiClient->getWebhookListenerService()->create($this->spaceId, $entity);
